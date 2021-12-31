b007cfac57e95bb8be6878e5ba1056fc4e545762
Victor Fernandez <victorhf2014@gmail.com>
2021-12-31 02:51:49 -0300

    add javaScript

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const enviarCorreo = document.getElementById('seccionUno');
+enviarCorreo.addEventListener('click', function (){
+   Swal.fire("el correo fue enviado correctamente...");
+});
+
+// Swal.fire({
+//     position: 'top-end',
+//     icon: 'success',
+//     title: 'Your work has been saved',
+//     showConfirmButton: false,
+//     timer: 1500
+//   })
