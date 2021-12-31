8b482bdac911aea837318df060f18405c8fa96d3
Victor Fernandez <victorhf2014@gmail.com>
2021-12-31 11:53:50 -0300

    page ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-const enviarCorreo = document.getElementById('seccionUno');
-enviarCorreo.addEventListener('click', function (){
-   Swal.fire("el correo fue enviado correctamente...");
+const enviarCorreo = document.getElementById("contacto");
+enviarCorreo.addEventListener("click", function () {
+  Swal.fire("Deseas contactarnos?...");
 });
+
+// const { value: email } = await Swal.fire({
+//   title: "Input email address",
+//   input: "email",
+//   inputLabel: "Your email address",
+//   inputPlaceholder: "Enter your email address",
+// });
+
+// if (email) {
+//   Swal.fire(`Entered email: ${email}`);
+//}
 
 // Swal.fire({
 //     position: 'top-end',
